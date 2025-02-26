8202240081e2d3f6c43e3347a2d93cd2539bc05c
ulug'bek <azizbekovulugbek8@gmail.com>
2025-02-26 11:23:07 +0500

    none

---
+++ index.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body");
+let rang1 = prompt("Yoqtirgan rangizni kiriting");
+let rang2 = prompt("Yoqtirgan rangizni kiriting");
+
+body.style.backgroundColor = rang1;
+body.style.color = rang2;
